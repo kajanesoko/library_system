57f48d8232023deaf98c42b2fabd1a1bfd831683
Jacob Mziya <jacobmziya@gmail.com>
2015-09-07 15:36:50 +0200

    Modified navigation links

--- db/migrate/20150903133459_alter_create_items.rb
+++
@@ -1,7 +0,0 @@
-class AlterCreateItems < ActiveRecord::Migration
-  def up
-  	change_table :item do |t|
-		t.string :status, :default => ""	
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903133459) do
+ActiveRecord::Schema.define(version: 20150907124116) do
+
+  create_table "borrows", force: true do |t|
+    t.string   "name"
+    t.integer  "numberrrr"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", force: true do |t|
     t.datetime "created_at"
